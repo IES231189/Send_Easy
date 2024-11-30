75841b0c6fd2e898126eaf512c5e2f65d1f8792e
231189alo <231189@ids.upchiapas.edu.mx>
2024-11-30 02:26:11 -0600

    actualizacion de rutas para usar modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { authGuard } from './guards/auth.guard';
 import { LoginComponentsComponent } from './auth/components/login-components/login-components.component';
 import { AdminAppComponent } from './role/admin/admin-app/admin-app.component';
+import { adminGuard } from './guards/admin.guard';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'admin', component: AdminAppComponent, canActivate: [authGuard] },
+  { path: 'admin', loadChildren: () => import('./role/admin/admin.module').then(m => m.AdminModule), canActivate: [adminGuard] },
   { path: 'login', component: LoginComponentsComponent },
   { path: '*', redirectTo: 'login' }
 
