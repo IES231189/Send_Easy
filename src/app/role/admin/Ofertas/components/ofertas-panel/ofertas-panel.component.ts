4d7c8dedd7b8156d1380d940b0eaaccd36d08ed0
231189alo <231189@ids.upchiapas.edu.mx>
2024-11-30 19:06:14 -0600

    implementacion de ofertas(entrada principal del modulo)

--- src/app/role/admin/Ofertas/components/ofertas-panel/ofertas-panel.component.ts
+++ src/app/role/admin/Ofertas/components/ofertas-panel/ofertas-panel.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+interface NavOption {
+  label: string;
+  route: string;
+}
+
 @Component({
   selector: 'app-ofertas-panel',
   templateUrl: './ofertas-panel.component.html',
-  styleUrl: './ofertas-panel.component.css'
+  styleUrls: ['./ofertas-panel.component.css']
 })
 export class OfertasPanelComponent {
+
+  adminNavOptions: NavOption[] = [
+
+    { label: 'Ver todas las ofertas', route: 'admin/ofertas/all' },
+    { label: 'Agregar Ofertas', route: '/admin/ofertas/add' },
+  ];
 
 }
