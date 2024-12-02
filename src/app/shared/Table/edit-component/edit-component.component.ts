2cab21a81603d1f141699af45b3d2676518dda72
231189alo <231189@ids.upchiapas.edu.mx>
2024-12-01 22:33:49 -0600

    Actualizacion de componentes reutilizables

--- src/app/shared/Table/edit-component/edit-component.component.ts
+++ src/app/shared/Table/edit-component/edit-component.component.ts
@@ -1,11 +1,11 @@
-import { Component , Input, Output , EventEmitter, OnChanges , SimpleChanges} from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-edit-component',
   templateUrl: './edit-component.component.html',
-  styleUrl: './edit-component.component.css'
+  styleUrls: ['./edit-component.component.css']
 })
-export class EditComponentComponent implements OnChanges{
+export class EditComponentComponent implements OnChanges {
   @Input() registroTable: any;
   @Output() GuardarCambios = new EventEmitter<any>();
 
@@ -13,13 +13,32 @@ export class EditComponentComponent implements OnChanges{
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['registroTable'] && changes['registroTable'].currentValue) {
-      this.editedData = { ...this.registroTable };
+      // Convertir las fechas al formato de entrada
+      const registro = changes['registroTable'].currentValue;
+      this.editedData = { ...registro };
+
+      // Si la fecha no está en formato correcto para el input date, la ajustamos
+      if (this.editedData.fecha_inicio) {
+        this.editedData.fecha_inicio = this.formatDateForInput(this.editedData.fecha_inicio);
+      }
+      if (this.editedData.fecha_fin) {
+        this.editedData.fecha_fin = this.formatDateForInput(this.editedData.fecha_fin);
+      }
     }
   }
 
+
+
   OnSave() {
+    if (!this.editedData || !this.editedData.id_oferta) {
+      console.error('El objeto a guardar no tiene un ID válido:', this.editedData);
+      return;
+    }
     this.GuardarCambios.emit(this.editedData);
   }
+
+
+
 
   OnCancel() {
     this.editedData = { ...this.registroTable };
@@ -27,5 +46,11 @@ export class EditComponentComponent implements OnChanges{
 
   getObjectKeys(obj: any): string[] {
     return Object.keys(obj);
+  }
+
+  private formatDateForInput(date: string): string {
+    // Asegúrate de que la fecha esté en formato YYYY-MM-DD
+    const parsedDate = new Date(date);
+    return parsedDate.toISOString().split('T')[0]; // Devuelve en formato YYYY-MM-DD
   }
 }
